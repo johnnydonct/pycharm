033958e30c67e6399a9669fb384fd1698927061c
johnnydonct <p_leonardo@ufms.br>
2025-09-18 13:35:18 -0400

    Update ex22.py

--- ex revisao/ex22.py
+++ ex revisao/ex22.py
@@ -2,4 +2,6 @@ bmn= float(input("Insira a base menor do seu trapézio: "))
 bma= float(input("Insira a base maior do seu trapézio: "))
 h= float(input("Insira a altura do seu trapézio: "))
 A = ((bma+bmn)*h)/2
+
 print(f"A área do trapézio é {A}")
+#a professora não queria que fizesse assim porque segundo ela, tinha outro jeito mais elaborado, mas esse é muito mais simples

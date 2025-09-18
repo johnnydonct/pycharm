78cc609b805db67467b9c7462fb7491d6e2ccc89
johnnydonct <p_leonardo@ufms.br>
2025-09-18 13:34:00 -0400

    Update ex21.py

--- ex revisao/ex21.py
+++ ex revisao/ex21.py
@@ -2,3 +2,4 @@ n = int(input("Entre com o tamanho dos catetos: "))
 c = input("Entre com o caractere a ser usado: ")
 for i in range (1, n+1):
     print(c*i)
+    #o programa vai imprimir um triângulo usando os caracteres escolhidos

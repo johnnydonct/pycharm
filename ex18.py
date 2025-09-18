646e3df2991d96a392ec272776298570965f33cc
johnnydonct <p_leonardo@ufms.br>
2025-09-18 10:30:43 -0400

    Update ex18.py

--- ex18.py
+++ ex18.py
@@ -1,5 +1,9 @@
 n = int(input("Insira um número inteiro positivo: "))
 soma = 0
 for i in range (1,n+1):
-    soma = 2*i**2+3*i**3+4*i**4
+    soma = (2*(i**2))+(3*(i**3))+(4*(i**4))+soma
+#     ou soma += (2*(i**2))+(3*(i**3))+(4*(i**4)) 
+#     += serve para somar a próxima soma à soma feita anteriormente, assim como em 
+#     "soma = algo + soma"
 print(soma)
+#111177 para 10, só pra ver se da certo

90b2c9bdddecaf204c12f2c9ac390ba9ad7aa2cb
johnnydonct <p_leonardo@ufms.br>
2025-09-22 15:03:01 -0400

    Update ex20.py

--- ex revisao/ex20.py
+++ ex revisao/ex20.py
@@ -1,7 +1,6 @@
 n = int(input("Insira um número inteiro positivo: "))
 soma = 0
 for i in range(1, n+1):
-    s =  + s
-print(s)
-
-#ainda não terminei esse
+    i = i 
+    soma = i/(1+n-i)+ soma #+1 pra não zerar o denominador
+print(soma)

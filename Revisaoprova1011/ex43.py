55671b236224f52a3befaf8bedeff2955612fbb8
johnnydonct <p_leonardo@ufms.br>
2025-11-10 18:35:42 -0400

    Implement matrix transposition and symmetry check
    Added functions to transpose a matrix and check symmetry.

---
+++ Revisaoprova1011/ex43.py
@@ -0,0 +1,32 @@
+def transpor_matriz(A):
+    if not A:
+        return []
+    num_linhas = len(A)
+    num_colunas = len(A[0])
+    AT = []
+    for j in range(num_colunas):
+        nova_linha = []
+        for i in range(num_linhas):
+            nova_linha.append(A[i][j])
+        AT.append(nova_linha)
+    return AT
+#--------------
+def sime(x, y):
+    if x == y:
+        print (f"A matriz {x} é simétrica")
+    else:
+        print(f"A matriz {x} não é simétrica")
+
+l = int(input("Entre com o numero de linhas: "))
+c = int(input("Entre com o numero de colunas: "))
+A = []
+for i in range(l):
+    en = []
+    for j in range(c):
+        en.append(int(input("Entre com o elemento da matriz: ")))
+    A.append(en)
+
+AT= transpor_matriz(A)
+print(f"A matriz original: {A}")
+print(f"A matriz transposta de A: {AT}")
+print(sime(A, AT))
